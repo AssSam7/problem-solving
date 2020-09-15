3dd1a6ec35375127daa2b46c5c72d622b9e4037c
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2020-09-15 18:48:02 +0530

    Adding the remove duplicates from the sorted linked list problem

---
+++ removeDuplicatesSortedLinkedList.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+  if (!head) return head;
+  let current = head;
+  while (current.next != null) {
+    var nextNode = current.next;
+    if (current.val == nextNode.val) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+  return head;
+};
