4595a7a1bcf418cd8037465691d5e884b7dfebe9
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2020-09-30 15:38:32 +0530

    Adding the Array problem - First Missing Positive

---
+++ Arrays/firstMissingPositive.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function (nums) {
+  let i = 1;
+  nums = nums
+    .filter((num, index) => num > 0 && nums.indexOf(num) == index)
+    .sort((a, b) => a - b);
+  while (i <= Math.max(...nums)) {
+    if (nums[i - 1] != i) {
+      return i;
+    }
+    i++;
+  }
+  return i;
+};
